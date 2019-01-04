715a68357970df797ee0d036391c81e12f706cd4
Brandon Brasson <rocksong622@gmail.com>
2019-01-04 19:59:16 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,9 @@
 # Build your say_hello method here
 
 def say_hello(name)
-  print "#{name}!"
+  print "say_hello#{name}!"
 end
 
- def hello()
+ def say_hello()
   puts "Hello Ruby Programmer!"
 end
